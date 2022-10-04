1fc082768a3ea27fc757b7cdd1b6cbd5008c6b3f
onur koc <onurkottz@gmail.com>
2022-10-04 10:30:44 +0300

    static files added

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Merhaba")
-})
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
 
 const port = 3000;
 
