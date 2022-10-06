27dfdadbee101140f0ad73132c11bfcfdf25b42a
onur koc <onurkottz@gmail.com>
2022-10-06 15:06:32 +0300

    controllers and pagination

--- app.js
+++ app.js
@@ -1,127 +1,48 @@
 const express = require('express');
-
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-// File upload:
-const fileUpload = require('express-fileupload');
-// Manipulating POST method for as PUT and DELETE
-const methodOverride = require('method-override');
-
-//Create a image file for database image.
-const fs = require('fs');
-const path = require('path');
+const fileUpload = require('express-fileupload'); // File upload:
+const methodOverride = require('method-override'); // Manipulating POST method for as PUT and DELETE
 const ejs = require('ejs');
-const Photo = require('./models/Photo');
-
+const photoController = require('./controllers/photoControllers'); // Import controllers
+const pageController = require('./controllers/pageControllers');
 
 const app = express();
-
 
 //Connecting database
 mongoose.connect('mongodb://localhost/pcat-test-db', {
   // useNewUrlParser: true,
   // useUnifiedTopology: true,
   // useFindAndModify: false,
+  //These all 3 are not necessary in new versions.
 });
 
-// Tempelate engine for dynamic files.
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); // Tempelate engine for dynamic files.
 
 // MIDDLEWARES
 
-// For create a server.
-app.use(express.static('public'));
-
-app.use('app')
-
+app.use(express.static('public')); // For create a server.
 // For to end the getting requests. (for POST method) urlcoded captures data in the URL. .json() converts data that captured in the URL to json formot.
-//app.use(express.urlencoded({ extend: true }));
+// app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-
-// File upload. encType="multipart/form-data" is necessary for uploads. Add this <form>.
-app.use(fileUpload());
-
-app.use(methodOverride('_method'), {
-  methods: ['POST', 'GET'],
-});
-
-
-
-
+app.use(fileUpload()); // File upload. encType="multipart/form-data" is necessary for uploads. Add this <form>.
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
 // ROUTES
 
-app.get('/', async (req, res) => {
-  // Sending datas to index.
-  const photos = await Photo.find({}).sort('-dateCreated');
-  res.render('index', {
-    photos,
-  });
-});
-
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-
-app.get('/add', (req, res) => {
-  res.render('add');
-});
-
-app.get('/photos/edit/:id', async (req, res) => {
-  const photo = await Photo.findOne({ _id: req.params.id });
-  res.render('edit', {
-    photo,
-  });
-});
-
-app.post('/photos', async (req, res) => {
-  // Create data. "/photos" is action name of form. /image is name that necessary for file upload
-  //console.log(req.files.image); this is for getting image detail
-  const uploadDir = 'public/uploads';
-
-  if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir);
-  }
-
-  let uploadedImage = req.files.image;
-  let uploadPath = __dirname + '/public/uploads/' + uploadedImage.name;
-
-  uploadedImage.mv(uploadPath, async () => {
-    await Photo.create({
-      ...req.body,
-      image: '/uploads/' + uploadedImage.name,
-    });
-    res.redirect('/');
-  });
-});
-
-app.get('/photo/:id', async (req, res) => {
-  // Sending details to details page.
-  const photo = await Photo.findById(req.params.id);
-  res.render('photo', {
-    photo,
-  });
-});
-
-app.put('/photos/:id', async (req, res) => {
-  // Update photo. DON'T FORGET THAT IN FORM TAG => action="/photos/<%= photo._id %>?_method=PUT"
-  const photo = await Photo.findOne({ _id: req.params.id });
-  photo.title = req.body.title;
-  photo.description = req.body.description;
-  photo.save();
-  res.redirect(`/`);
-});
-
-app.delete('/photos/:id', async (req, res) => {
-  // Delete photo. As first remove the photo from folder and then remove it from database.
-  const photo = await Photo.findOne({ _id: req.params.id });
-  let deletedImage = __dirname + '/public' + photo.name;
-  fs.unlinkSync(deletedImage);
-  await Photo.findByIdAndRemove(req.params.id);
-  res.redirect(`/`);
-});
-
+app.get('/', photoController.getAllPhotos);
+app.get('/photos/:id', photoController.getDetailPage);
+app.post('/photos', photoController.createPhoto);
+app.put('/photos/:id', photoController.updatePhoto);
+app.delete('/photos/:id', photoController.deletePhoto);
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+app.get('/photos/edit/:id', pageController.getEditPage);
 
 // PORT
 
---
+++ controllers/photoControllers.js
@@ -0,0 +1,74 @@
+const Photo = require('../models/Photo');
+const fs = require('fs'); //Create a image file for database image.
+
+exports.getAllPhotos = async (req, res) => {
+  // Below code is for pagination.
+  const page = req.query.page || 1;
+  const photoPerPage = 3;
+  const totalPhoto = await Photo.find().countDocuments();
+  const photos = await Photo.find({})
+    .sort('-dateCreated')
+    .skip((page - 1) * photoPerPage)
+    .limit(photoPerPage);
+  res.render('index', {
+    photos,
+    current: page,
+    pages: Math.ceil(totalPhoto / photoPerPage),
+  });
+
+  // Below colde is just write index page and send data.
+  /* const photos = await Photo.find({}).sort('-dateCreated');
+  res.render('index', {
+    photos,
+  }); */
+
+  // Sending all datas to index.
+};
+
+exports.getDetailPage = async (req, res) => {
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo', {
+    photo,
+  });
+
+  // Sending details to details page.
+};
+
+exports.createPhoto = async (req, res) => {
+  const uploadDir = 'public/uploads';
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+  let uploadedImage = req.files.image;
+  let uploadPath = __dirname + '/../public/uploads/' + uploadedImage.name;
+  uploadedImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadedImage.name,
+    });
+    res.redirect('/');
+  });
+
+  // Create data. "/photos" is action name of form. /image is name that necessary for file upload
+  //console.log(req.files.image); this is for getting image detail
+};
+
+exports.updatePhoto = async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.title = req.body.title;
+  photo.description = req.body.description;
+  photo.save();
+  res.redirect(`/`);
+
+  // Update photo. DON'T FORGET THAT IN FORM TAG => action="/photos/<%= photo._id %>?_method=PUT"
+};
+
+exports.deletePhoto = async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  let deletedImage = __dirname + '/../public' + photo.name;
+  fs.unlinkSync(deletedImage);
+  await Photo.findByIdAndRemove(req.params.id);
+  res.redirect(`/`);
+
+  // Delete photo. As first remove the photo from folder and then remove it from database. DON'T FORGET THAT IN TAG => action or href="/photos/<%= photo._id %>?_method=DELETE"
+};
